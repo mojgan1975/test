6cf98c54400abafa9e5cbbd8d9fac8483587d443
Mojgan <mojgan@kidocode.com>
2017-05-26 05:40:41 +0000

    Table of elements

---
+++ test2.py
@@ -0,0 +1,22 @@
+class element :
+    def __init__(self,elementName,atomicNumber):
+
+        self.elementName=elementName
+        self.atomicNumber=atomicNumber
+
+hydrogen=element('hydrogen','1.008')
+helium=element('helium','4.002')
+
+
+hydrogen.elementName
+helium.elementName
+
+hydrogen.atomicNumber
+helium.atomicNumber
+
+print(hydrogen.elementName)
+print(helium.elementName)
+
+print(hydrogen.atomicNumber)
+print(helium.atomicNumber)
+        
